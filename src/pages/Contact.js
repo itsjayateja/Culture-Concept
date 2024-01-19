afd763c3d5d27c27e213868b21ab1bdcd72c54c8
Mohammed Ameer <mohdameer788@gmail.com>
2024-01-19 18:05:39 +0530

    contact-page-fixed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,11 @@ import ContactImage from "../assets/Contact-Image.jpg";
 import "../pages/Contact.css";
 import { FaMobileAlt } from "react-icons/fa";
 import { FiMapPin, FiMail } from "react-icons/fi";
-
+import { useFormik } from "formik";
+import * as Yup from 'yup'
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Loader2 } from "lucide-react";
 const Result = ({ success, message }) => {
   return <p className={success ? "confirm" : "error"}>{message}</p>;
 };
@@ -13,7 +17,7 @@ const Result = ({ success, message }) => {
 const Contact = () => {
   const [result, setResult] = useState({ success: false, message: "" });
   const form = useRef();
-
+  const [isLoading,setIsLoading]=useState(false)
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -58,6 +62,53 @@ const Contact = () => {
 
     e.target.reset();
   };
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Name is Required'),
+    phoneNumber: Yup.string().required('Phone Number is Required'),
+    email: Yup.string().required('Email is Required'),
+    subject: Yup.string().required('Subject is Required'),
+    message: Yup.string().required('Message is Required'),
+
+});
+
+    const formik = useFormik({
+
+        initialValues: {
+            phoneNumber:'',
+            name:'',
+            email:'',
+            subject:'',
+            message:''
+        },
+        validationSchema: validationSchema,
+        onSubmit: async(values) => {
+          setIsLoading(true)
+       
+          try {
+            console.log("here are values",values)
+            const response = await axios.post(
+              "https://script.google.com/macros/s/AKfycbyRxDUeJyFJXgUoc73TMEunlZfOnaer0yhnAXRUpV6AOsqTrmRWtGBahL1pjSbx7uDb/exec",
+              new URLSearchParams({...values}),
+              {
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+              }
+            );
+            if(response.status===200){
+              console.log(response)
+              toast.success("Sent Successfully")
+            }
+            } catch (error) {
+             
+              toast.error("Something Went Wrong!")
+              console.log(error)
+                 
+            }finally{
+              setIsLoading(false)
+            }
+        },
+      });
   return (
     <Layout title={"Culture Concept/Contact"}>
       <section className="About">
@@ -114,57 +165,80 @@ const Contact = () => {
               <div className="Cantact-form">
                 <div className="contact-heading">Get In Touch</div>
 
-                <form ref={form} onSubmit={sendEmail}>
+                <form ref={form} >
                   <div className="mb-3 form-input">
                     <input
                       type="text"
+                      onChange={(e)=>formik.setFieldValue("name",e.target.value)}
                       className="form-control"
                       id="username"
                       placeholder="Username"
                       name="from-name"
                     />
+                    {formik.touched.name && formik.errors.name ? (
+                                    <div style={{color:'red'}} className=" text-red-600">{formik.errors.name}</div>
+                                ) : null}
                   </div>
                   <div className="mb-3 form-input">
                     <input
                       type="phone"
+                      onChange={(e)=>formik.setFieldValue("phoneNumber",e.target.value)}
                       placeholder="Phone Number"
                       className="form-control"
                       id="exampleInputEmail1"
                       name=""
                     />
+                    {formik.touched.phoneNumber && formik.errors.phoneNumber ? (
+                                    <div style={{color:'red'}} className=" text-red-600">{formik.errors.phoneNumber}</div>
+                                ) : null}
                   </div>
                   <div className="mb-3 form-input">
                     <input
                       type="email"
+                      onChange={(e)=>formik.setFieldValue("email",e.target.value)}
                       placeholder="Email"
                       className="form-control"
                       id="exampleInputEmail1"
                       name="from-email"
                     />
+                    {formik.touched.email && formik.errors.email ? (
+                                    <div style={{color:'red'}} className=" text-red-600">{formik.errors.email}</div>
+                                ) : null}
                   </div>
                   <div className="mb-3 form-input">
                     <input
                       type="text"
+                      onChange={(e)=>formik.setFieldValue("subject",e.target.value)}
                       className="form-control"
                       id="username"
                       placeholder="Subject"
                       name="from-name"
                     />
+                    {formik.touched.subject && formik.errors.subject ? (
+                                    <div style={{color:'red'}} className=" text-red-600">{formik.errors.subject}</div>
+                                ) : null}
                   </div>
                   <div className="mb-3 form-input form">
                     <textarea
                       type="text"
+                      onChange={(e)=>formik.setFieldValue("message",e.target.value)}
                       name="message"
                       className="form-control"
                       placeholder="message"
                     />
+                    {formik.touched.message && formik.errors.message ? (
+                                    <div style={{color:'red'}} className=" text-red-600">{formik.errors.message}</div>
+                                ) : null}
                   </div>
-                  <div className="contact-btn">
-                    <input type="submit" value="Send" className="btn2" />
+                  <div style={{marginBottom:"20px"}} className="contact-btn">
+                    <button disabled={isLoading}  onClick={(e)=>{
+                      e.preventDefault()
+                      formik.submitForm()
+                    }}  value="Send" className="btn2">{isLoading && <Loader2 size={12} className="animate-spin" />} Send</button>
                   </div>
-                  <div className="row">
+                  {/* <div className="row">
                     <Result success={result.success} message={result.message} />
-                  </div>
+                  </div> */}
                 </form>
               </div>
             </div>
